bf08ae9d54a0f4e3daca33d02fbf3415e20d5449
Matt <mab48671@uga.edu>
2017-02-02 13:16:00 -0500

    more fixes

--- login.php
+++ login.php
@@ -15,24 +15,22 @@
         $sql = "SELECT * FROM p2users WHERE name = '$username' and pw = '$password'";
         $userAccount = $conn->query($sql);
         if($userAccount->rowCount() > 0){
-        session_start();
-        $_SESSION["username"] = $username;
-        
-        /*
-        Determines the current time
-        */
-        date_default_timezone_set('America/New_York');
-        $timeStamp = new DateTime();
-        $timeOfLogin = date("D d M Y, g:i:s a", $timeStamp->getTimeStamp());
-            
-        if(!isset($_COOKIE['time'])){
+            session_start();
+            $_SESSION["username"] = $username;
+
+            /*
+            Determines the current time
+            */
+            date_default_timezone_set('America/New_York');
+            $timeStamp = new DateTime();
+            $timeOfLogin = date("D d M Y, g:i:s a", $timeStamp->getTimeStamp());
+
             setcookie('time', $timeOfLogin);
             header("Location: todolist.php"); // login successful so redirect to the TO-DO LIST
             die();
-            }
         }
         else{
-            header("Location: index.html"); // login unsuccessful so redirect to login page
+            header("Location: index.php"); // login unsuccessful so redirect to login page
             die();
         }
         
--- logout.php
+++ logout.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
     session_destroy();
-    setcookie('time','', -1);
-    header("Location: index.html");
+    header("Location: index.php");
     die();
 ?>
